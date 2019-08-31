217e679470414c880574f30c9dba71e52ca64d0a
meny770 <menachem1414@gmail.com>
2019-09-01 00:14:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-app.use(cookieParser())
 app.use(cors);
 
 app.use(express.static(path.join(__dirname, './build')))
